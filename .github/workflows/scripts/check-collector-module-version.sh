d8cc7eda3a01a524d6a53731b4d75d99ad01ceb9
Janik <10290002+led0nk@users.noreply.github.com>
2024-04-01 01:12:21 +0200

    shellcheck

---
+++ .github/workflows/scripts/check-collector-module-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# verifies if the collector components are using the main core collector version
+# as a dependency.
+#
+echo $BETA_modules
